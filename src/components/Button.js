a316c82dda825a3ed7a06a93f3eb02f50892bf0c
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-17 22:45:40 +1000

    setup context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
-    return <button className="ui primary">Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'Jamaa';
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
