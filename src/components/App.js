a4ffe69a97f74524c692d38568507d18dcd7a79a
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-17 22:08:39 +1000

    added all the componenets

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
   state = {language: 'english'};
@@ -21,6 +22,7 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange('urdu')}
           />
         </div>
+        <UserCreate />
       </div>
     );
   }
