919d49657502bac8ff9e65245d89d56b774d5ca5
Yasir Malik <yasirrafiq786@gmail.com>
2020-08-17 22:03:40 +1000

    made usercreate component

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {language: 'english'};
+
+  onLanguageChange = (language) => {
+    this.setState({language});
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag pk"
+            onClick={() => this.onLanguageChange('urdu')}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
